68db566d0f4b32771034564dc4b8c40dab012d7f
mariCrisCV <maria.cris.castaneda.v@gmail.com>
2024-09-02 17:20:15 -0500

    en cuentas se crean los archivos buscarCuenta y agregarCuenta, se editan los nombres de los integrantes y de las cajas en el archivo cuentas.html

--- TrabajoGrupal2/cuentas.js
+++ TrabajoGrupal2/cuentas.js
@@ -1,58 +1,74 @@
-cuentas=[
-    {numeroCuenta:"02234567", cedula:"1714616123",nombre:"Juan",apellido:"Perez",saldo:0.0},
-    {numeroCuenta:"02345211",cedula:"1281238233",nombre:"Felipe",apellido:"Caicedo",saldo:0.0}
+let cuentas = [
+    { numeroCuenta: "02234567", cedula: "1714616123", nombre: "Juan", apellido: "Perez", saldo: 0.0 },
+    { numeroCuenta: "02345211", cedula: "1281238233", nombre: "Felipe", apellido: "Caicedo", saldo: 0.0 }
 ]
 
-cargar=function(){
+cargar = function () {
     mostrarComponente("divCuentas");
     ocultarComponente("divMovimientos");
     ocultarComponente("divTransacciones");
     mostrarCuentas();
 }
 
-mostrarCuentas=function(){
+mostrarCuentas = function () {
     /*
         Muestra en pantalla una tabla con la información de todas las cuentas del arreglo.
         Columnas: NUMERO CUENTA, NOMBRE, SALDO
         En la columna NOMBRE concatenar el nombre y el apellido
     */
-        let cmpTabla = document.getElementById("tablaCuentas");
-        let contenidoTabla = "<table><tr>" +
-            "<th>NUMERO CUENTA</th>" +
-            "<th>NOMBRE</th>" +
-            "<th>SALDO</th>" +
-            "</tr>";
-        let elementoCuenta;
-        for (let i = 0; i < cuentas.length; i++) {
-            elementoCuenta = cuentas[i];
-            contenidoTabla +=
-                "<tr><td>" + elementoCuenta.numeroCuenta + "</td>"
-                + "<td>" + elementoCuenta.nombre +" "+ elementoCuenta.apellido + "</td>"
-                + "<td>" + elementoCuenta.saldo + "</td>"
-                + "</tr>"
-        }
-        contenidoTabla += "</table>"
-        cmpTabla.innerHTML = contenidoTabla;
+    let cmpTabla = document.getElementById("tablaCuentas");
+    let contenidoTabla = "<table><tr>" +
+        "<th>NUMERO CUENTA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>SALDO</th>" +
+        "</tr>";
+    let elementoCuenta;
+    for (let i = 0; i < cuentas.length; i++) {
+        elementoCuenta = cuentas[i];
+        contenidoTabla +=
+            "<tr><td>" + elementoCuenta.numeroCuenta + "</td>"
+            + "<td>" + elementoCuenta.nombre + " " + elementoCuenta.apellido + "</td>"
+            + "<td>" + elementoCuenta.saldo + "</td>"
+            + "</tr>"
+    }
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla;
 }
 
 /*
     Busca la cuenta en el arreglo en función del número de cuenta,
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
-buscarCuenta=function(numeroCuenta){
-
+buscarCuenta = function (numeroCuenta) {
+    let elementoCuenta;
+    let cuentaEncontrada = null;
+    for (let i = 0; i < cuentas.length; i++) {
+        elementoCuenta = cuentas[i];
+        if (elementoCuenta.numeroCuenta == numeroCuenta) {
+            cuentaEncontrada = elementoCuenta;
+            break;
+        }
+    }
+    return cuentaEncontrada;
 }
 
 /*
     Agrega una cuenta al arreglo, solamente si no existe otra cuenta con el mismo numero.
     No retorna nada
 */
-agregarCuenta=function(cuenta){
+agregarCuenta = function (cuenta) {
     //Si ya existe mostrar un alert CUENTA EXISTENTE
     //Si se agrega, mostrar un alert CUENTA AGREGADA
+    let cuentaBusqueda = buscarCuenta(cuenta);
+    if (cuentaBusqueda==null){
+        cuentas.push(cuenta);
+        alert("CUENTA AGREGADA");
+    }else{
+        alert("CUENTA EXISTENTE");
+    }
 }
 
-agregar=function(){
+agregar = function () {
     //Toma los valores de las cajas de texto, sin validaciones
     //Crea un objeto cuenta y agrega los atributos con los valores de las cajas respectivas
     //Invoca a agregarCuenta
